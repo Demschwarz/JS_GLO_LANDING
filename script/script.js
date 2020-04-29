4c32cd37846fffbf26e7bc0740ba64fc06ed2b2c
Ilya <chernyi.ilya.00@gmail.com>
2020-04-29 19:04:13 +0300

    lesson 23 usual probably done

--- script/script.js
+++ script/script.js
@@ -263,6 +263,69 @@ window.addEventListener('DOMContentLoaded', function() {
         elem.addEventListener('input', () => {
             elem.value = elem.value.replace(/\D/, '');
         })
-    })
+    });
+
+    // калькулятор
+
+    const calc = (price = 100) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
+        
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+            
+            if (calcCount.value > 1) {
+                countValue *= 1 + (calcCount.value - 1) * 0.1;
+            }
+
+            if (!!calcDay.value && (+calcDay.value < 5)) {
+                dayValue = 2;
+            } else if (!!calcDay.value && (+calcDay.value < 10)) {
+                dayValue = 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * typeValue *  squareValue * countValue * dayValue;
+            }
+
+            // const rotate = () => {
+            //     let counter = 0;
+            //     const peres = setInterval(() => {
+            //         counter += 1;
+            //         totalValue.textContent = counter;
+            //         if (counter >= total) {
+            //             clearInterval(peres);
+            //         }
+            //     }, 6);
+            // }
+            // rotate();
+        }
+
+        calcBlock.addEventListener('change', (event) => {
+            const target = event.target;
+
+            // if (target.matches('.calc-type') || target.matches('.calc-square') ||
+            //  target.matches('.calc-day') || target.matches('.calc-count')) {
+            //      console.log(1);
+            //  }
+            // if (target === calcType || target === calcSquare || target === calcDay || target === calcCount) {
+            //     console.log(1);
+            // }
+
+            if (target.matches('select') || target.matches('input')) {
+                countSum();
+            }
+        })
+    };
+
+    calc(100);
     
 });
